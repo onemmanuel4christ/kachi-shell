3876b1ab18555bc198467304db33c8b9411a72dd
onemmnuel4christ <onemmanuel4christ@gmail.com>
2023-10-20 23:00:06 +0100

    fixes

--- add_cd_func.c
+++ add_cd_func.c
@@ -1,32 +1,28 @@
-```c
 #include "main.h"
 
 /**
- * print_cd_1 - Outputs an error message when the cd argument is invalid
- *              e.g., cd -asd22
- * @vars: Pointer to vars_t structure
- * Return: void
- * Prints a message in the format shown below.
+ * print_cd_1 - Prints error when cd argument in invalid
+ *              i.e. cd -asd22
+ * @vars: vars parameter to include
+ * Return: Always return void and print a messaje as shown below.
  */
 void print_cd_1(vars_t *vars)
 {
-    print_str("-bash: cd: ", 1);
-    print_str(vars->array_tokens[1], 1);
-    print_str(": invalid option", 0);
-    print_str("cd: usage: cd [-] [--]", 0);
+	print_str("-bash: cd: ", 1);
+	print_str(vars->array_tokens[1], 1);
+	print_str(": invalid option", 0);
+	print_str("cd: usage: cd [-] [--]", 0);
 }
 
 /**
- * print_cd_2 - Outputs an error message when the cd argument doesn't exist.
- *              e.g., cd das7d32
- * @vars: Pointer to vars_t structure
- * Return: void
- * Prints a message in the format shown below.
+ * print_cd_2 - Prints error message when cd argument doesn'n exist.
+ *              i.e. cd das7d32
+ * @vars: vars parameter to include
+ * Return: Always return void and print a messaje as shown below.
  */
 void print_cd_2(vars_t *vars)
 {
-    print_str("-bash: cd: ", 1);
-    print_str(vars->array_tokens[1], 1);
-    print_str(": Not a directory", 0);
+	print_str("-bash: cd: ", 1);
+	print_str(vars->array_tokens[1], 1);
+	print_str(": Not a directory", 0);
 }
-```
